396cdc8a16eb4e7df48897ef5949df08dfca147e
nelsesepta <nelseseptahenidar@gmail.com>
2021-10-05 12:50:31 +0700

    deploy

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function renderHTML(res, path) {
+  return fs.readFile(path, function (err, data) {
+    if (err) return err;
+    res.end(data);
+  });
+}
+
+function htmlDOM(path, callback) {
+  fs.readFile(path, function (err, data) {
+    if (err) return err;
+    callback(data.toString());
+  });
+}
+
+function errorFile(res, html) {
+  fs.writeFile("./views/error.html", html, function (err) {
+    if (err) return err;
+    renderHTML(res, "./views/error.html");
+  });
+}
+function LoadingFile(res, html) {
+  fs.writeFile("./views/loading.html", html, function (err) {
+    if (err) return err;
+    renderHTML(res, "./views/loading.html");
+  });
+}
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  renderHTML(res, "./views/index.html");
+});
+
+app.get("/download", async function (req, res) {
+  try {
+    htmlDOM("./views/index.html", function (data) {
+      const { document } = new JSDOM(`${data}`).window;
+      document.body.querySelector(".fancy-spinner").style.display = "flex";
+
+      const html = document.documentElement.outerHTML;
+      LoadingFile(res, html);
+    });
+    const youtubeLink = req.query.url;
+    const info = await ytdl.getBasicInfo(youtubeLink);
+    const title = info.player_response.videoDetails.title;
+    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+    ytdl(youtubeLink, {
+      format: "mp4",
+    }).pipe(res);
+  } catch (err) {
+    htmlDOM("./views/index.html", function (data) {
+      const { document } = new JSDOM(`${data}`).window;
+      const errorText = document.body.querySelector("span.error-text");
+      errorText.textContent = `Sorry.. video url ${req.query.url} not found, please check your video url`;
+      const html = document.documentElement.outerHTML;
+      errorFile(res, html);
+    });
+  }
+});
+
+app.get("/mp3", async function (req, res) {
+  try {
+    const youtubeLink = req.query.url;
+    const info = await ytdl.getBasicInfo(youtubeLink);
+    const title = info.player_response.videoDetails.title;
+
+    res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+
+    ytdl(youtubeLink, {
+      format: "mp3",
+      filter: "audioonly",
+    }).pipe(res);
+  } catch (err) {
+    htmlDOM("./views/index.html", function (data) {
+      const { document } = new JSDOM(`${data}`).window;
+      const errorText = document.body.querySelector("span.error-text");
+      errorText.textContent = `Sorry.. video url ${req.query.url} not found, please check your video url`;
+      const html = document.documentElement.outerHTML;
+
+      errorFile(res, html);
+    });
+  }
+});
+
+// 404 Page
+app.use((req, res) => {
+  renderHTML(res, "./views/404.html");
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Running at localhost:3000")
+);
---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+const inputURL = document.querySelector("input.url");
+const btnDownload = document.querySelectorAll(".btn-download");
+
+function clickedBtnDownload(e) {
+  const url = inputURL.value;
+  if (e.target.classList.contains("video")) {
+    downloadVideo(url);
+  } else if (e.target.classList.contains("mp3")) {
+    downloadAudio(url);
+  }
+}
+
+inputURL.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const url = inputURL.value;
+    console.log(url.length);
+    downloadVideo(url);
+  }
+});
+
+function downloadVideo(url) {
+  if (url) {
+    window.location.href = `${window.location.origin}/download?url=${url}`;
+    return;
+  }
+
+  alert("Please insert your Video URL");
+}
+
+function downloadAudio(url) {
+  if (url) {
+    window.location.href = `${window.location.origin}/mp3?url=${url}`;
+    return;
+  }
+
+  alert("Please insert your Video URL");
+}
+
+btnDownload.forEach((button) =>
+  button.addEventListener("click", clickedBtnDownload)
+);
